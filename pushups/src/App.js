40a3fc8a9a4c3df8cfb2a5117f53a85c6ba55cd0
Sahil Kalra <sahilkalra4321@gmail.com>
2021-07-12 01:01:02 -0700

    Adds userInputs for the Landing Page

--- pushups/src/App.js
+++ pushups/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Pushups Masters </h1>
+      <h4>
+        Remember to enter your daily entries to stay active for your total
+        amount otherwise your total count will be removed after 72 hours!
+      </h4>
+      <div className="row g-2">
+        <div className="col-md">
+          <div className="form-floating">
+            <input className="username form-control" id="usernameInput"></input>
+            <label for="usernameInput">Username</label>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="form-floating">
+            <input className="totalReps form-control" id="userReps"></input>
+            <label for="userReps">Number of Reps</label>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="form-floating">
+            <input className="userNumber form-control" id="userNumber"></input>
+            <label for="userNumber">Phone Number (Optional)</label>
+          </div>
+        </div>
+        <div className="col-md">
+          <button className="submit btn btn-outline-secondary">Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
